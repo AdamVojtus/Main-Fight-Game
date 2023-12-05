31d03344876460d4ca512b17400629b4c87182f2
Adam Vojtuš <146973705+AdamVojtus@users.noreply.github.com>
2023-12-05 20:49:21 +0100

    Fight Game 1.3

---
+++ AdamkoLesson1 (1)/AdamkoLesson1/AdamkoLesson1/Program.cs
@@ -0,0 +1,26 @@
+﻿using AdamkoLesson1;
+
+var player1 = new Player("Anton", "Sutiak", 1983, 20, 10);
+var player2 = new Player("Adam", "Vojtus", 2008, 20, 10);
+
+var listOfPlayers = new List<Player>
+{
+    player1, 
+    player2,
+};
+
+var game = new Game(listOfPlayers, false, 10);
+
+var newPlayer = new Player("Jon", "Snow", 1800, 100);
+game.AddPlayer(newPlayer);
+
+game.Start();
+
+while (game.IsGameRunning)
+{
+    Console.WriteLine("Score of players are: []");
+}
+
+Console.WriteLine("Game over!");
+Console.ReadLine();
+
